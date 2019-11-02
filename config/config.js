bcaddac02ba2554a28e9fb88fb68483d7d62d922
Vedant <mainvedant.vns123@gmail.com>
2019-11-02 13:42:56 +0530

    deploy

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/whitepanda'
+    // url: 'mongodb://localhost:27017/whitepanda'
+    url:'mongodb://whitepandauser:white123@ds161455.mlab.com:61455/whitepanda'
 }
